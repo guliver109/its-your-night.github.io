c0c92643e3bfeae6573c83588b9ff971277f17f1
Marko Gusic <markogusic@aol.com>
2018-09-05 13:14:19 -0700

    test

---
+++ assets/javascript/app.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+
+    function buildQuery() {
+        //variable declaration
+        var queryURL = "https://maps.googleapis.com/maps/api/js?key=" + APIKey + "&callback=initMap";
+        var APIKey = ;
+    }
+
+});
