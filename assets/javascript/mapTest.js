f118355bab1a833c8be50ad277091892d75c8682
Ana <aparakaki17@gmail.com>
2018-09-11 23:09:33 -0700

    direction display on html

--- assets/javascript/mapTest.js
+++ assets/javascript/mapTest.js
@@ -56,6 +56,7 @@ $(document).ready(function () {
             this.googleRestaurant();
             this.googleVenue();
             this.getDirection();
+            // this.displayDirections();
         }
       });
     }
@@ -119,6 +120,21 @@ $(document).ready(function () {
         for (var i = 0; i < response.routes[0].legs.length; i++) {
           placesObject.route[i] = response.routes[0].legs[i];
         }
+
+        for (var j = 0; j < response.routes[0].legs.length; j++) {
+          var newDiv = $("<div>");
+          for (var i = 0; i <response.routes[0].legs[j].steps.length; i++) {
+            // var newP = $("<p>").text(response.routes[0].legs[j].steps[i].html_instructions);
+            newDiv.append(response.routes[0].legs[j].steps[i].html_instructions);
+            // $("#routing-instructions").append(response.routes[0].legs[j].steps[i].html_instructions);
+            console.log(response.routes[0].legs[j].steps[i].html_instructions);
+            var brTag = $("<br>");
+          }
+          $("#routing-instructions").append(newDiv);
+        }
+
+
+
         var directionsDisplay = new google.maps.DirectionsRenderer();
         directionsDisplay.setMap(placesObject.map);
         var directionsService = new google.maps.DirectionsService();
@@ -146,19 +162,23 @@ $(document).ready(function () {
 
 //---------------------------------------------------------------------------
 //display all directions to html
-    placesObject.displayDirections() = function() {
-      for (var i = 0; i < this.route.length; i++) {
-        var newDiv = $("<div");
-        for (var j = 0; i <this.route.steps.lenth; j++) {
-          var newP = $("<p>").text(`${this.route.steps[i].html_instructions}`);
-          newDiv.append(newp);
-        }
-        $("#routing-instructions").append(newDiv);
-      }
-    }
+    // placesObject.displayDirections = function() {
+    //   console.log("displaying route object");
+    //   // console.log(this.route);
+    //   // console.log(this.route.length);
+    //   for (var j = 0; j < 3; j++) {
+    //     var newDiv = $("<div>");
+    //     for (var i = 0; i <this.route[j].steps.length; i++) {
+    //       var newP = $("<p>").text(`${this.route[j].steps[i].html_instructions}`);
+    //       newDiv.append(newP);
+    //       console.log(this.route[j].steps[i].html_instructions);
+    //     }
+    //     $("#routing-instructions").append(newDiv);
+    //   }
+    // }   
 
 
-    
+    // placesObject.displayDirections();
 //getting coordinates from user input address
     function codeAddress(address) {
       console.log("address function");
@@ -168,7 +188,7 @@ $(document).ready(function () {
         url: `https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${APIKey}`,
         method: "GET"
     }).then(function(response) {
-      console.log(response);
+      // console.log(response);
       cArray.push(response.results[0].geometry.location.lat);
       cArray.push(response.results[0].geometry.location.lng);
     });
@@ -185,18 +205,17 @@ $(document).ready(function () {
 
     placesObject.getCoordinates = function(start, end) {
       if(start !== "") {
-        console.log("populating array")
         userStart = codeAddress(start);
-        this.restLat = userStart[0]
-        this.currentPositionLatitude = userStart[0];
-        this.currentPositionLongitude = userStart[1];
-        console.log(userStart);
-        console.log(this.restLat);
+        placesObject.restLat = userStart[0]
+        placesObject.currentPositionLatitude = userStart[0];
+        placesObject.currentPositionLongitude = userStart[1];
+        console.log(userStart[0]);
+        console.log(placesObject.restLat);
       }
       if (end !== "") {
         userEnd = codeAddress(end);
-        this.endPositionLatitude = userEnd[0];
-        this.endPositionLongitude = userEnd[1];
+        placesObject.endPositionLatitude = userEnd[0];
+        placesObject.endPositionLongitude = userEnd[1];
         console.log(placesObject.endPositionLatitude);
       }
       // placesObject.rerouting();
@@ -227,7 +246,7 @@ $(document).ready(function () {
       // }
       // // placesObject.rerouting();
     })
-     console.log(placesObject);
+    //  console.log(placesObject);
 
   
  });
